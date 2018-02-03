d3e6e2e7bd16b8fedf24305f638fb2e1fc69cb8a
mayubo <mayubo@passiontec.cn>
2018-02-03 11:12:04 +0800

    新建工程
    xcode9.2版本

---
+++ YUPhotoBrowserDemo/YUPhotoBrowserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YUPhotoBrowserDemo
+//
+//  Created by mayubo on 2018/2/3.
+//  Copyright © 2018年 裕帝哥哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YUPhotoBrowserDemo/YUPhotoBrowserDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YUPhotoBrowserDemo
+//
+//  Created by mayubo on 2018/2/3.
+//  Copyright © 2018年 裕帝哥哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
